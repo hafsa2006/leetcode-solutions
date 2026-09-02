3c2ec46032b74bbb49a861ed4174445618076f1f
Hafsa Rumaiza <hafsarumaiza43@gmail.com>
2026-09-02 22:30:21 +0530

    Time: 0 ms (100%), Space: 9 MB (18.26%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else {
+                // Stack is empty → no opening bracket to match
+                if (st.empty()) {
+                    return false;
+                }
+
+                if (st.top() == '(' && s[i] == ')') {
+                    st.pop();
+                }
+                else if (st.top() == '{' && s[i] == '}') {
+                    st.pop();
+                }
+                else if (st.top() == '[' && s[i] == ']') {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
