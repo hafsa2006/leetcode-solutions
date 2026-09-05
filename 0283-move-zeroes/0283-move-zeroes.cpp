0f54ddf1ef29e5d5bf26fcabc560b369fa238c87
Hafsa Rumaiza <hafsarumaiza43@gmail.com>
2026-09-05 10:52:15 +0530

    Time: 224 ms (5.07%), Space: 23.8 MB (55.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,9 +3,9 @@ public:
     void moveZeroes(vector<int>& nums) {
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==0 && nums[j]!=0){
+                if(nums[i]==0){
                     swap(nums[i],nums[j]);
-                    break;
+                   
                 }
             }
         }
