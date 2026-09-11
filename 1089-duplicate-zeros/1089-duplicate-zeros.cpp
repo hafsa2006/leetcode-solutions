c3941d24adbe1060928a6a985ee24e7998cecd5d
Hafsa Rumaiza <hafsarumaiza43@gmail.com>
2026-09-11 12:10:28 +0530

    Time: 0 ms (100%), Space: 14.6 MB (6.78%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                ans.push_back(0);
+                ans.push_back(0);
+            }else{
+                ans.push_back(arr[i]);
+            }
+        }
+        ans.resize(n);
+        arr=ans;
+    }
+};
